05eef003fc3a09094f286a957f9c009d90be4e94
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-05 21:18:28 +0200

    3rd to end

--- 013-lists.py
+++ 013-lists.py
@@ -1,7 +1,8 @@
 from tools import separator, section
 
+colors = ["red", "black", "blue", "green", "yellow"]
+
 section('len')
-colors = ["red", "blue", "green"]
 print(f"There are {len(colors)} colors.")
 
 section('various types')
@@ -10,3 +11,11 @@ print(f"The third item is of type: {type(items[2])}")
 separator()
 for item in items:
 	print(f"Value {item} is of type: {type(item)}")
+
+section('last item')
+print(f"the last item is {colors[-1]}")
+
+section('range')
+print(f"items with index numbers 1-3: {colors[1:4]}") # last index number is not included
+print(f"from second to end: {colors[1:]}")
+print(f"first three: {colors[:3]}") # last index number is not included
