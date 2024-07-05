0d584e1603c4e1b5f647a4604ce6f2922d411081
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-05 17:48:54 +0200

    title

--- tools.py
+++ tools.py
@@ -2,4 +2,11 @@ def separator():
 	print('---')
 
 def section(title):
-	print(f'\n=== {title.upper()} ===============================================')
+	sideLength = 10
+	wrapLine = "=" * (sideLength * 2 + 2 + len(title))
+	side = "=" * sideLength
+	print('')
+	print(wrapLine)
+	print(f'{side} {title.upper()} {side}')
+	print(wrapLine)
+	print('')
