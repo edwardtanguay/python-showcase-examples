e12dd2a43ca323eb3a7bfdc342cf92c2dd45f726
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-05 12:47:43 +0200

    random

---
+++ 008-collections.py
@@ -0,0 +1,11 @@
+# list --> like a JavaScript array
+scores = [1,2,3]
+
+# tuple --> like a JavaScript array that can't be changed
+ages = (1,2,3)
+
+# set --> like a JavaScript Set
+temperatures = {1,2,3}
+
+# dictionary --> like a JavaScript object
+person = {"name": "Hans", "age": 45}
---
+++ 009-random.py
@@ -0,0 +1,6 @@
+import random
+
+age = random.randrange(18,65)
+heightInCentimeters = random.randrange(150,190)
+
+print(f"He is {age} years old and {heightInCentimeters} cm tall.")
