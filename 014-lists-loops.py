07f63e6079cf249c3b53ce972c6784332fbce444
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-05 21:50:48 +0200

    list comprehension

--- 014-lists-loops.py
+++ 014-lists-loops.py
@@ -9,3 +9,12 @@ for planet in planets:
 section('iterate through items using index')
 for i in range(len(planets)):
 	print(f"{i+1}. {planets[i]}")
+
+section('while')
+i = len(planets) - 1
+while i >= 0:
+	print(f"#{i+1} = {planets[i]}")
+	i -= 1
+
+section('list comprehension')
+[print(planet) for planet in planets]
