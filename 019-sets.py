dd1ae1bd08395764a2c49f5792543085c59d7a28
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-06 13:43:01 +0200

    sets

--- 019-sets.py
+++ 019-sets.py
@@ -24,3 +24,43 @@ newPlants = {"Peony", "Daisy"}
 print(plants)
 plants.update(newPlants)
 print(plants)
+
+section('remove random item')
+print(plants)
+removed = plants.pop()
+print(f'removed "{removed}"')
+print(plants)
+
+# for is same
+
+section('all items from both')
+plants2 = {"Lily", "Fern", "Snap Dragons"}
+matches = plants.union(plants2)
+print(plants)
+print(plants2)
+separator()
+print(matches)
+
+section('only the duplicates')
+plants2 = {"Lily", "Fern", "Snap Dragons"}
+print(plants)
+print(plants2)
+matches = plants.intersection(plants2)
+separator()
+print(matches)
+
+section('only items that were in first set, but not in second')
+plants2 = {"Lily", "Fern", "Snap Dragons"}
+print(plants)
+print(plants2)
+matches = plants.difference(plants2)
+separator()
+print(matches)
+
+section('all differences')
+plants2 = {"Lily", "Fern", "Snap Dragons"}
+print(plants)
+print(plants2)
+matches = plants.symmetric_difference(plants2)
+separator()
+print(matches)
