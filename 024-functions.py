8148cd704c6bce0eb20a63770fcb5ecb362ce2a2
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-06 18:15:19 +0200

    func

--- 024-functions.py
+++ 024-functions.py
@@ -3,7 +3,6 @@ from tools import separator, section
 section('arguments')
 def getFullName(first, last):
   return f"{first} {last}"
-
 fullName = getFullName("Roger", "Gargoyle")
 print(fullName)
 
@@ -14,5 +13,16 @@ def printInfos(*infos):
   print(f"There are {len(infos)} infos.")
   for i,info in enumerate(infos):
     print(f"{i+1}. {info}")
-
 printInfos('rock', 'tree')
+
+section('keyword arguments') # also known as kwargs
+def getMessage(name, age):
+  return f"{name} is {age} years old."
+print(getMessage('Harold', 33))
+print(getMessage(age = 55, name = "Alice"))
+
+section('default parameter')
+def displayName(name = '(unknown)'):
+  print(f"The name is {name}.")
+displayName('Richard')
+displayName()
