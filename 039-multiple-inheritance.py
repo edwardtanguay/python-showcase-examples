50fca18ef6f10d51933038db32782231eff7699c
Edward Tanguay (windows) <edward@tanguay.info>
2024-12-26 07:57:13 +0100

    multiple

--- 039-multiple-inheritance.py
+++ 039-multiple-inheritance.py
@@ -1,18 +1,33 @@
 class Camera:
+	def __init__(self, name):
+		self.name = name
 	def capture_photo(self):
-		print("took picture")
+		print(self.name + " is taking a picture")
 	def info(self):
-		print("this is a camera")
+		print(self.name + " is a camera")
 
-class Stereo:
+class MusicPlayer:
+	def __init__(self, name):
+		self.name = name
 	def play_music(self):
-		print("playing music")
+		print(self.name + " is playing music")
 	def info(self):
-		print("this is a stereo")
+		print(self.name + " is a music player")
 
-canonEos = Camera()
+class MultipurposeDevice(Camera, MusicPlayer):
+	def __init__(self, name):
+		self.name = name
+
+canonEos = Camera("Canon EOS 5D Mark IV")
 canonEos.capture_photo()
+canonEos.info()
 
-sonyCmt = Stereo()
+sonyCmt = MusicPlayer("Sony CMT-SBT100")
 sonyCmt.play_music()
+sonyCmt.info()
+
+iPhone = MultipurposeDevice("Apple iPhone 15 Pro Max")
+iPhone.play_music()
+iPhone.capture_photo()
+iPhone.info()
 
